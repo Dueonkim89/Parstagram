9908e0506022ee300eab5f900e004652e20444f8
unknown <dueonkim89@Yahoo.com>
2022-03-24 19:51:53 -0700

    initial commit

---
+++ app/src/main/java/com/example/codepath_instagram/LoginActivity.kt
@@ -0,0 +1,75 @@
+package com.example.codepath_instagram
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.parse.ParseUser
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // check if user logged in
+        // take them to main activity
+        if (ParseUser.getCurrentUser() != null) {
+            goToMainActivity()
+        }
+
+
+        findViewById<Button>(R.id.login_button).setOnClickListener{
+            val username = findViewById<EditText>(R.id.username).text.toString()
+            val password = findViewById<EditText>(R.id.password).text.toString()
+            loginUser(username, password)
+        }
+
+        findViewById<Button>(R.id.signupBtn).setOnClickListener{
+            val username = findViewById<EditText>(R.id.username).text.toString()
+            val password = findViewById<EditText>(R.id.password).text.toString()
+            signUpUser(username, password)
+        }
+    }
+
+    private  fun signUpUser(username: String, password: String) {
+        // Create the ParseUser
+        val user = ParseUser()
+
+        // Set fields for the user to be created
+        user.setUsername(username)
+        user.setPassword(password)
+
+        user.signUpInBackground { e ->
+            if (e == null) {
+                // User has successfully made a new account, navigate to main activity
+                goToMainActivity()
+
+            } else {
+                // Sign up didn't succeed. Look at the ParseException
+                // to figure out what went wrong
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun loginUser(username: String, password: String) {
+        ParseUser.logInInBackground(username, password, ({ user, e ->
+            if (user != null) {
+                // Hooray!  The user is logged in.
+                Log.i("DK123", "You are logged in!")
+                goToMainActivity()
+            } else {
+                e.printStackTrace()
+            }})
+        )
+    }
+
+    private fun goToMainActivity() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        // close out login activity
+        finish()
+    }
+}
