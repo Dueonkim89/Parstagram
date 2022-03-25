7f4d3c6894a66894ce2e61140849045f51b726b7
dueonkim89 <dueonkim89@yahoo.com>
2022-03-24 23:12:16 -0700

    parse data object and allow querying

--- app/src/main/java/com/example/codepath_instagram/MainActivity.kt
+++ app/src/main/java/com/example/codepath_instagram/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.codepath_instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        queryPosts()
     }
+
+    // QUery for all posts in our server
+    fun queryPosts() {
+        // Specify which class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+
+        // Find all post objects
+        query.include(Post.KEY_USER)
+        query.findInBackground(object : FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if (e != null) {
+                    // something went wrong
+                    Log.e("DK_ERROR", "Error fetching posts")
+                } else {
+                    if (posts != null) {
+                        for (post in posts) {
+                            Log.i("Success_post", "Post is: " + post.getDescription() + post.getUser()?.username)
+                        }
+                    }
+                }
+            }
+
+        })
+    }
+
 }
